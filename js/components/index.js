2ff59ff97f6df61b4e0b2b3db5be30f5b02c7426
Jason Tilley <tilley_jason@hotmail.com>
2025-06-20 11:43:02 -0400

    restructured modular folders

--- js/components/index.js
+++ js/components/index.js
@@ -3,4 +3,5 @@ export { renderRSVP } from './renderRSVP.js';
 export { renderSuggestions } from './renderSuggestions.js';
 export { renderAdminTools } from './renderAdminTools.js';
 export { renderSelectedGames } from './renderSelectedGames.js';
+export { renderGameNights } from './render.js';
 
