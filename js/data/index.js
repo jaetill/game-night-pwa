694dcc8e512bafb51e3b2261f616bf67d175108c
Jason Tilley <tilley_jason@hotmail.com>
2025-06-21 08:08:33 -0400

    trying to modal game selection

--- js/data/index.js
+++ js/data/index.js
@@ -2,3 +2,4 @@ export { currentUser, isAdmin, ownedGames, setOwnedGames } from './state.js';
 export { loadGameNights, syncGameNights } from './storage.js';
 export { fetchOwnedGames } from './bgg.js';
 export { filterGames } from './gameFilters.js';
+
