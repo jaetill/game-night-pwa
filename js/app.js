b35e142f8cd4adab97253485e932c45ea561bb24
jaetill <tilley_jason@hotmail.com>
2025-06-25 19:12:11 -0400

    trying to fix signups for selected games

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 import { loadGameNights, fetchOwnedGames} from './data/index.js';
-import { currentUser, isAdmin } from './auth/auth.js';
+import { getCurrentUser, isAdmin } from './auth/auth.js';
 
 import { renderApp } from './components/render.js';
 import { setupEventListeners } from './events/events.js';
 
 async function init() {
-  if (!currentUser) {
+  if (!getCurrentUser()) {
     console.error('No current user found. Please log in.');
     return;
   }
 
-  const adminStatus = await isAdmin(currentUser);
+  const adminStatus = await isAdmin(getCurrentUser());
   const nights = await loadGameNights();
   await fetchOwnedGames("jaetill");
 
-  renderApp({ nights, isAdmin: adminStatus, currentUser });
+  renderApp({ nights, isAdmin: adminStatus, getCurrentUser() });
   // Initialize the app with game nights and admin tools if applicable
   // This will render the game nights and admin tools based on the current user and their role    
   setupEventListeners();
