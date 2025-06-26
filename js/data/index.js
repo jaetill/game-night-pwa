e713a36327f87436993d7ef5bab67edf8fdb1ef8
jaetill <tilley_jason@hotmail.com>
2025-06-26 00:15:43 -0400

    adding host attribute

--- js/data/index.js
+++ js/data/index.js
@@ -1,5 +1,5 @@
 export { ownedGames, setOwnedGames } from './state.js';
-export { loadGameNights, syncGameNights, saveGameNights } from './storage.js';
+export { loadGameNights, saveGameNights } from './storage.js';
 export { fetchOwnedGames } from './bgg.js';
 export { filterGames } from './gameFilters.js';
 
