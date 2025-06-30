68159776969b38b19dea283c9f268864daf3cd7d
jaetill <tilley_jason@hotmail.com>
2025-06-30 16:29:42 -0400

    Migrate app to Vite + Cognito login flow

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,8 @@ import { loadGameNights, fetchOwnedGames } from './data/index.js';
 import { renderApp } from './components/render.js';
 import { setupEventListeners } from './events/events.js';
 import { Amplify } from 'aws-amplify';
-//import  Auth  from '@aws-amplify/auth';
+import * as Auth from 'aws-amplify/auth';
 
-
-//Amplify.addPluggable(Auth);
-
-// 🔧 Configure Amplify Auth (you can also hardcode config inline if not using aws-exports.js)
 Amplify.configure({
   Auth: {
     region: 'us-east-2',
@@ -22,14 +18,9 @@ Amplify.configure({
     },
   },
 });
-// This configuration sets up Amplify Auth with the necessary parameters for your Cognito User Pool and OAuth settings.
-// This function handles the login process by redirecting to the hosted UI
-// and is called when the user clicks the login button.
-// If you are using aws-exports.js, you can import it directly
-// and pass it to Amplify.configure() instead of hardcoding the config.
 
 const handleLogin = () => {
-  Auth.federatedSignIn(); // This takes the user to the hosted UI
+  Auth.federatedSignIn();
 };
 
 async function init() {
@@ -43,10 +34,9 @@ async function init() {
 
     renderApp({ nights, currentUser: user });
     setupEventListeners();
-
   } catch (err) {
     console.warn('User not signed in. Redirecting to login...');
-    Auth.federatedSignIn(); // Redirects to hosted UI
+    Auth.federatedSignIn();
   }
 }
 
